95b8073b9ae64ad200355fda77b1ccfb34add171
esteban <estemantillacar@gmail.com>
2025-10-28 06:12:45 -0500

    feat :sparkles: first aggregate

--- campus_music/aggregations.mongodb.js
+++ campus_music/aggregations.mongodb.js
@@ -1,1 +1,31 @@
 // agregaciones
+// 1. ¿Cuántos estudiantes se inscribieron por sede en el último mes?
+db.Sedes.aggregate(
+
+)
+
+// 2. ¿Cuáles son los cursos más demandados en cada sede?
+
+
+
+// 3. ¿Cuál es el ingreso total generado por inscripciones en cada sede?
+
+
+
+// 4. ¿Qué profesor tiene más estudiantes asignados?
+
+
+
+// 5. ¿Qué instrumento es el más reservado?
+
+
+
+// 6. Mostrar el **historial de cursos de un estudiante** (fecha, sede, curso, profesor, nivel, costo).
+
+
+
+// 7. Listar los **cursos actualmente en ejecución** en cada sede.
+
+
+
+// 8. Detectar cursos que **excedieron el cupo** permitido en algún momento.
